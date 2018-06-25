956bc7b271486a1bf823a36fa3e5cdd4ee270df9
Joy <joy@Joys-MacBook-Pro.local>
2018-06-25 14:21:08 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const GrpcClient = require("./client/grpc");
 const HttpClient = require("./client/http");
+const GrpcWebClient = require("./client/grpc-web");
+const Types = require("./protocol/api/api_pb");
 
 module.exports = {
   GrpcClient,
   HttpClient,
+  GrpcWebClient,
+  Types
 };
